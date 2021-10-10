723abc5ba73a4e42a016798e29cf7f6c6852a21a
rtimbers <rtimbers@hotmail.com>
2021-10-10 12:24:22 +1100

    Update aks_cluster.tf

--- 1-terraform/aks_cluster.tf
+++ 1-terraform/aks_cluster.tf
@@ -57,10 +57,10 @@ resource "azurerm_kubernetes_cluster" "aks_k2" {
 
 output "client_certificate" {
   value = "${azurerm_kubernetes_cluster.aks_k2.kube_config.0.client_certificate}"
-  sensitive: true
+  sensitive = true
 }
 
 output "kube_config" {
   value = "${azurerm_kubernetes_cluster.aks_k2.kube_config_raw}"
-  sensitive: true
+  sensitive = true
 }
